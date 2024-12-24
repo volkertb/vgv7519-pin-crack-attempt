af170ef9b80ba537fa674feb1c3904523ce19768
Volkert de Buisonjé <volkertb@users.noreply.github.com>
2024-12-24 20:32:52 +0100

    feat: Generate SHA-1 hash from "hello, world" string, and add helper functions that convert hashes back and forth between byte arrays and hex strings

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@
 # make sure there is a <tab> character on the indented line!
 all: vgv7519-pin-cracker.c
 	mkdir -p build
-	gcc -g -Wall -o build/vgv7519-pin-cracker vgv7519-pin-cracker.c
+	gcc -lcrypto -g -Wall -o build/vgv7519-pin-cracker vgv7519-pin-cracker.c
 
 clean:
 	rm -rf build/*
--- vgv7519-pin-cracker.c
+++ vgv7519-pin-cracker.c
@@ -3,8 +3,68 @@
 //
 
 #include <stdio.h>
+#include <string.h>
+#include <openssl/sha.h>
+
+/**
+ * Code below with thanks to https://stackoverflow.com/a/51263027
+ * @param binary_hash
+ * @param hex_hash
+ */
+void sha1_hash_to_hex_string(unsigned char binary_hash[SHA_DIGEST_LENGTH], unsigned char hex_hash[SHA_DIGEST_LENGTH*2])
+{
+    int i = 0;
+    for (i=0; i < SHA_DIGEST_LENGTH; i++) {
+        sprintf((char*)&(hex_hash[i*2]), "%02x", binary_hash[i]);
+    }
+}
+
+/**
+ * ode below with thanks to https://stackoverflow.com/a/13194749
+ * @param hex_hash
+ * @param bytearray
+ */
+void hex_string_to_sha1_hash(unsigned char hex_hash[40], unsigned char bytearray[20])
+{
+    char *hexstring = hex_hash;
+    int i;
+    uint8_t str_len = strlen(hexstring);
+
+    for (i = 0; i < (str_len / 2); i++) {
+        sscanf(hexstring + 2*i, "%02x", &bytearray[i]);
+        printf("bytearray %d: %02x\n", i, bytearray[i]);
+    }
+}
 
 int main(void)
 {
-    printf("hello, world\n");
+    const unsigned char data[] = "hello, world\n";
+
+    size_t length = strlen(data);
+
+    unsigned char binary_hash[SHA_DIGEST_LENGTH];
+    SHA1(data, length, binary_hash);
+
+    unsigned char hex_hash[SHA_DIGEST_LENGTH*2];
+
+    sha1_hash_to_hex_string(binary_hash, hex_hash);
+
+    // And print to stdout
+    printf("Hash: %s\n", hex_hash);
+
+    printf("Binary hash 1: %s\n", binary_hash);
+
+    unsigned char bytearray[SHA_DIGEST_LENGTH];
+
+    hex_string_to_sha1_hash(hex_hash, bytearray);
+
+    printf("Binary hash 2: %s\n", bytearray);
+
+    if (memcmp(binary_hash, bytearray, sizeof(SHA_DIGEST_LENGTH)))
+    {
+        printf("Byte arrays should be identical, but they are not.");
+    } else
+    {
+        printf("Byte arrays should be identical, and they are.");
+    }
 }
